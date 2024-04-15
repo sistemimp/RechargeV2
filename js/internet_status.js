1058c4b90107791886270c11ef338536bb096122
sistemimp <161827941+sistemimp@users.noreply.github.com>
2024-04-15 11:41:40 +0200

    preload&ISBugFix
    Signed-off-by: sistemimp <161827941+sistemimp@users.noreply.github.com>

--- js/internet_status.js
+++ js/internet_status.js
@@ -1,27 +1,41 @@
 const sito = "https://app.reweicoli.it/"
-const  log =require ('electron-log/renderer')
+const log = require('electron-log/renderer')
 
 async function internet_status() {
-
-    fetch(`${sito}/kernel/Aj?gp=recharge&action=checkOnline`).then(response => response.json()).then(result => {
-        
-        if(result.online){
-            log.info("Internet Status - Accesso:"+global.accesso)
-            document.getElementById("internet_status").innerHTML = 'online'
-            document.getElementById('internet_check').style.display = "none"
-            if(global.accesso=="false"){
+    try {
+        let IS = await fetch(`${sito}/kernel/Aj?gp=recharge&action=checkOnline`).then(response => response.json()).then(result => {
+            console.log("IS - " + result.online)
+            if (result.online) {
+                log.info("Internet Status - Accesso:" + global.accesso)
+                document.getElementById("internet_status").innerHTML = 'online'
+                document.getElementById('internet_check').style.display = "none"
+                if (global.accesso == "false") {
+                    document.getElementById('exe').style.display = "none"
+                } else {
+                    document.getElementById('exe').style.display = "block"
+                }
+                log.info('Internet Status - Online')
+                global.isOnline="true"
+            } else {
+                document.getElementById("internet_status").innerHTML = 'offline'
+                document.getElementById('internet_check').style.display = "block"
+                document.getElementById('login').style.display = "none"
                 document.getElementById('exe').style.display = "none"
-            }else{
-                document.getElementById('exe').style.display = "block"
+                document.getElementById('newversion').style.display = "none"
+                document.getElementById('noaccess').style.display = "none"
+                log.info('Internet Status - Offline')
+                global.isOnline="false"
             }
-            log.info('Internet Status - Online')
-        }else{
-            document.getElementById("internet_status").innerHTML = 'offline'
-            document.getElementById('internet_check').style.display = "block"
-            document.getElementById('login').style.display = "none"
-            document.getElementById('exe').style.display = "none"
-            log.info('Internet Status - Offline')
-        }
-    })
+        })
+    } catch (error) {
+        document.getElementById("internet_status").innerHTML = 'offline'
+        document.getElementById('internet_check').style.display = "block"
+        document.getElementById('login').style.display = "none"
+        document.getElementById('newversion').style.display = "none"
+        document.getElementById('noaccess').style.display = "none"
+        document.getElementById('exe').style.display = "none"
+        log.info('Internet Status - Offline')
+        global.isOnline="false"
+    }
 }
 
